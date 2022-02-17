722db47a7fd8ebb0538778400ad386f6c120e45d
Nguyen <nguyen2697s@gmail.com>
2022-02-17 15:13:48 +0700

    add ss3_mang_va_phuong_thuc.bai_tap.them_phan_tu_vao_mang

--- src/ss3_mang_va_phuong_thuc/bai_tap/them_phan_tu_vao_mang/InsertElement.java
+++ src/ss3_mang_va_phuong_thuc/bai_tap/them_phan_tu_vao_mang/InsertElement.java
@@ -9,8 +9,19 @@ public class InsertElement {
         System.out.println("Nhập giá trị X muốn thêm: ");
         Scanner scanner = new Scanner(System.in);
         int X = Integer.parseInt(scanner.nextLine());
-        System.out.println("Nhập vị trí index muốn chèn: ");
-        int index = Integer.parseInt(scanner.nextLine());
-
+        int index;
+        do {
+            System.out.println("Nhập vị trí index muốn chèn: ");
+            index = Integer.parseInt(scanner.nextLine());
+            if (index < 0 || index > array.length) {
+                System.out.println("Vị trí không hợp lệ. Nhập lại");
+            }
+        } while (index < 0 || index > array.length);
+        System.arraycopy(array, index, array, index + 1, array.length - 1 - index);
+        array[index] = X;
+        System.out.print("Mảng sau khi thêm là: ");
+        for(int item : array) {
+            System.out.printf("%-3d",item);
+        }
     }
 }
