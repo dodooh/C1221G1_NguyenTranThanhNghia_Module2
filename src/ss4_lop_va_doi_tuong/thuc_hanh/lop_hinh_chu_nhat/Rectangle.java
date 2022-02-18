e15e7019baf25aeddc91e76469244eaf1a304869
Nguyen <nguyen2697s@gmail.com>
2022-02-18 13:28:58 +0700

    add ss4_lop_va_doi_tuong.thuc_hanh.lop_hinh_chu_nhat

---
+++ src/ss4_lop_va_doi_tuong/thuc_hanh/lop_hinh_chu_nhat/Rectangle.java
@@ -0,0 +1,28 @@
+package ss4_lop_va_doi_tuong.thuc_hanh.lop_hinh_chu_nhat;
+
+public class Rectangle {
+
+    double width;
+    double height;
+
+    public Rectangle() {
+
+    }
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width + this.height) * 2;
+    }
+
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+}
