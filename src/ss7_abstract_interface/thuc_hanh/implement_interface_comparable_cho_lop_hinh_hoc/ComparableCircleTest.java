876b6dbf31411f97df9af3ba86b510c93f1186c4
Nguyen <nguyen2697s@gmail.com>
2022-02-23 23:59:22 +0700

    add ss7_abstract_interface.thuc_hanh.implement_interface_comparator_cho_lop_hinh_hoc & ss7_abstract_interface.thuc_hanh.implement_interface_comparable_cho_lop_hinh_hoc;

---
+++ src/ss7_abstract_interface/thuc_hanh/implement_interface_comparable_cho_lop_hinh_hoc/ComparableCircleTest.java
@@ -0,0 +1,24 @@
+package ss7_abstract_interface.thuc_hanh.implement_interface_comparable_cho_lop_hinh_hoc;
+
+import java.util.Arrays;
+
+public class ComparableCircleTest {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[3];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Arrays.sort(circles);
+
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
---
+++ src/ss7_abstract_interface/thuc_hanh/implement_interface_comparator_cho_lop_hinh_hoc/CircleComparatorTest.java
@@ -0,0 +1,26 @@
+package ss7_abstract_interface.thuc_hanh.implement_interface_comparator_cho_lop_hinh_hoc;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class CircleComparatorTest {
+    public static void main(String[] args) {
+        Circle[] circles = new Circle[3];
+        circles[0] = new Circle(3.6);
+        circles[1] = new Circle();
+        circles[2] = new Circle(3.5, "indigo", false);
+
+        System.out.println("Pre-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Comparator circleComparator = new CircleComparator();
+        Arrays.sort(circles, circleComparator);
+
+        System.out.println("After-sorted:");
+        for (Circle circle : circles) {
+            System.out.println(circle);
+        }
+    }
+}
