fb0a6e31a46fe7eb7ed0d8bdf9995ecd978b4455
Nguyen <nguyen2697s@gmail.com>
2022-02-24 23:19:25 +0700

    add ss8_clean_code_refactoring.thuc_hanh

---
+++ src/ss8_clean_code_refactoring/thuc_hanh/refactoring_doi_ten_bien_va_tach_hang/Calculator.java
@@ -0,0 +1,26 @@
+package ss8_clean_code_refactoring.thuc_hanh.refactoring_doi_ten_bien_va_tach_hang;
+
+public class Calculator {
+    public static final char ADDITION = '+';
+    public static final char SUBTRACTION = '-';
+    public static final char MULTIPLICATION = '*';
+    public static final char DIVISION = '/';
+
+    public static int calculate(int firstOperand, int secondOperand, char operator) {
+        switch (operator) {
+            case ADDITION:
+                return firstOperand + secondOperand;
+            case SUBTRACTION:
+                return firstOperand - secondOperand;
+            case MULTIPLICATION:
+                return firstOperand * secondOperand;
+            case DIVISION:
+                if (secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new RuntimeException("Can not divide by 0");
+            default:
+                throw new RuntimeException("Unsupported operation");
+        }
+    }
+}
