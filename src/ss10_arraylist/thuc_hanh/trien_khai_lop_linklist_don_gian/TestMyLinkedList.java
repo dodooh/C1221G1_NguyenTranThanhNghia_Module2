674a91ff2cf3acd42d3b5ddcb10fb2ad9d7b60f1
Nguyen <nguyen2697s@gmail.com>
2022-02-27 22:52:27 +0700

    add ss10_arraylist.thuc_hanh

---
+++ src/ss10_arraylist/thuc_hanh/trien_khai_lop_linklist_don_gian/TestMyLinkedList.java
@@ -0,0 +1,15 @@
+package ss10_arraylist.thuc_hanh.trien_khai_lop_linklist_don_gian;
+
+public class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,20);
+        ll.printList();
+    }
+}
