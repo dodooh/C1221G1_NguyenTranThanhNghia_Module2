ec1e0c70f151647bfe002c358c2d9a86f22dfd9e
Nguyen <nguyen2697s@gmail.com>
2022-03-01 00:04:45 +0700

    add ss11_stack_queue.thuc_hanh.trien_khai_stack_dua_tren_linkedlist

---
+++ src/ss11_stack_queue/thuc_hanh/trien_khai_stack_dua_tren_linkedlist/MyGenericStack.java
@@ -0,0 +1,31 @@
+package ss11_stack_queue.thuc_hanh.trien_khai_stack_dua_tren_linkedlist;
+
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList<T>();
+    }
+
+    public void push(T element) {
+        stack.addFirst(element);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public boolean isEmpty() {
+        return stack.size() == 0;
+    }
+}
