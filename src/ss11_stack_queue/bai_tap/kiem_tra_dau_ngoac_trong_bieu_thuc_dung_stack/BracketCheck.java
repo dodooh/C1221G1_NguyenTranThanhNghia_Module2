f7a571efc30fb8713420b0d90432f65e9359fdf8
Nguyen <nguyen2697s@gmail.com>
2022-03-01 15:55:57 +0700

    fix ss11_stack_queue.bai_tap.kiem_tra_dau_ngoac_trong_bieu_thuc_dung_stack

--- src/ss11_stack_queue/bai_tap/kiem_tra_dau_ngoac_trong_bieu_thuc_dung_stack/BracketCheck.java
+++ src/ss11_stack_queue/bai_tap/kiem_tra_dau_ngoac_trong_bieu_thuc_dung_stack/BracketCheck.java
@@ -5,20 +5,28 @@ import java.util.Stack;
 public class BracketCheck {
 
     public static void main(String[] args) {
-        System.out.println(isValidBracket("s * (s – a) * (s – b) * (s – c)"));
-        System.out.println(isValidBracket("(– b + (b2 – 4*a*c)^0.5) / 2*a"));
-        System.out.println(isValidBracket("s * (s – a) * (s – b * (s – c)"));
+        System.out.println(isValidBracket("{s * (s – a) * (s – b) * (s – c)"));
+        System.out.println(isValidBracket("{[– b + (b*b – 4*a*c)^0.5] / 2 * a}"));
+        System.out.println(isValidBracket("[s * (s – ]a) * (s – b * (s – c)"));
         System.out.println(isValidBracket("s * (s – a) * s – b) * (s – c)"));
-        System.out.println(isValidBracket("(– b + (b^2 – 4*a*c)^(0.5/ 2*a))"));
+        System.out.println(isValidBracket("[– b + (b^2 – 4*a*c)^(0.5/ 2*a)]"));
     }
 
     private static boolean isValidBracket(String str) {
         Stack<Character> stack = new Stack<>();
         for (char c : str.toCharArray()) {
-            if (c == '(') {
-                stack.push('(');
-            } else if (c == ')') {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
                 if (stack.isEmpty()) {
+                    return false;
+                }
+                char checkedBracket = stack.peek();
+                // kiểm tra dấu đóng ngoặc với mở ngoặc có tương ứng không
+                // nếu KHÔNG có cặp nào tương ứng thì trả về false;
+                if (!((c == ')' && checkedBracket == '(') ||
+                    (c == ']' && checkedBracket == '[') ||
+                    (c == '}' && checkedBracket == '{'))) {
                     return false;
                 }
                 stack.pop();
