3005e1a0c1092a4c7823717def4a3d5659c73d0b
Nguyen <nguyen2697s@gmail.com>
2022-03-01 22:41:57 +0700

    remove demo

--- src/demo/static_am/static1/Calculator.java
+++
@@ -1,7 +0,0 @@
-package demo.static_am.static1;
-
-public class Calculator {
-    public static int sum(int a, int b) {
-        return a + b;
-    }
-}
--- src/demo/static_am/static1/Cat.java
+++
@@ -1,8 +0,0 @@
-package demo.static_am.static1;
-
-public class Cat {
-    void can() {
-        Dog chihuahua = new Dog("Caz", 5,20);
-//        chihuahua.bark();
-    }
-}
