102fe4f13834a8bc653fef336841da6d539fd511
Nguyen <nguyen2697s@gmail.com>
2022-03-10 01:17:42 +0700

    add ss17_binary_file_serialization.thuc_hanh

---
+++ src/ss17_binary_file_serialization/thuc_hanh/doc_va_ghi_file_nhi_phan_cho_dssv/Student.java
@@ -0,0 +1,54 @@
+package ss17_binary_file_serialization.thuc_hanh.doc_va_ghi_file_nhi_phan_cho_dssv;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    private int id;
+
+    private String name;
+
+    private String address;
+
+    public Student() {
+    }
+
+    public Student(int id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", address='" + address + '\'' +
+            '}';
+    }
+}
